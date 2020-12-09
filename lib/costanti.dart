da0dd72ad2185e53e5cb58d4ce224aed99f6d27e
albyrings <alberto04@giallo.it>
2020-12-09 23:07:42 +0100

    commit

--- lib/costanti.dart
+++ lib/costanti.dart
@@ -7,7 +7,8 @@ const top1 = Image(
   height: 150,
 );
 List<String> titoli = [
-  "Pubblicazione graduatorie definitive esperti madrelingua per sezioni Cambridge IGCSE",
+  "Iscrizioni 2021-22 – Open days al Liceo Scacchi il 18 Dicembre 2020, il 15 e 22 Gennaio 2021"
+      "Pubblicazione graduatorie definitive esperti madrelingua per sezioni Cambridge IGCSE",
   "Assenze di massa",
   "Ubicazione classi ed elenco classi con attività didattiche solo parzialmente in presenza",
   'Avviso per gli iscritti alle classi prime 2020-21',
@@ -18,7 +19,8 @@ List<String> titoli = [
   'Cerimonia di consegna dei fondi #ScacchiBatteCovid'
 ];
 List<String> contenuti = [
-  "Pubblicazione graduatorie definitive esperti madrelingua per sezioni Cambridge IGCSE",
+  "In vista delle iscrizioni alle prime classi per l’anno scolastico 2021-22, il Liceo Scacchi organizza tre giornate di Open days online per il 18 Dicembre 2020, il 15 e il 22 Gennaio 2021, dalle ore 16,30 alle ore 19,00, nel corso delle quali il Dirigente scolastico e i docenti presenteranno l’offerta formativa, le attività didattiche e gli ambienti della scuola. riporta di seguito il link per la partecipazione agli Open Days.https://sites.google.com/liceoscacchibari.it/orienta-scacchi-20-21/"
+      "Pubblicazione graduatorie definitive esperti madrelingua per sezioni Cambridge IGCSE",
   "Si comunica a tutti gli studenti che l’Istituto non autorizza né giustifica assenze di massa. L’eventuale decisione di assentarsi per partecipare a manifestazioni e/o scioperi si configura come scelta personale con relativa assunzione di responsabilità per la decisione presa. Pertanto, tale tipo di assenza è soggetta a sanzione.",
   'Si pubblicano in allegato l’ubicazione di tutte le classi del Liceo (Allegato 1) e l’elenco delle tredici classi che, fino al 7 novembre, svolgeranno le attività didattiche solo parzialmente in presenza (Allegato 2). Nella relativa tabella, in corrispondenza di ogni classe, è indicato il numero di alunni che, secondo una turnazione settimanale, seguiranno le lezioni a distanza. Il primo turno andrà da giovedì 24 a sabato 26 settembre, mentre nelle settimane successive ogni turno andrà dal lunedì al sabato.A partire dal 9 novembre, invece, le classi indicate nella tabella proseguiranno le attività didattiche totalmente in presenza, fino al 22 dicembre.Per l’individuazione degli alunni che nella fase iniziale dovranno partecipare a distanza alle lezioni, si seguirà l’ordine alfabetico dell’elenco di classe. Gli interessati, comunque, riceveranno una comunicazione personale tramite email, nella quale saranno fornite le istruzioni operative per effettuare il collegamento a distanza tramite l’applicazione “Meet” di Google.https://liceoscacchibari.edu.it/wp-content/uploads/2020/09/Allegato-2.-Classi-parzialmente-in-presenza-settembre-novembre-2020.pdf',
   'Avviso per gli iscritti alle classi prime 2020-21',
